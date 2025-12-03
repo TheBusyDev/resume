daecfe1f74ef40ba0e047f2f272fa28f4273fc71
Pietro Businaro <pietro.businaro@mail.polimi.it>
2025-12-03 13:39:50 +0100

    Add html generation

--- generate_resume.sh
+++ generate_resume.sh
@@ -4,3 +4,10 @@ pandoc resume.md \
        -o resume.pdf \
        --css=style.css \
        --pdf-engine=weasyprint
+
+pandoc resume.md \
+       -o resume.html \
+       --css=style.css \
+       --standalone
+
+exit 0
