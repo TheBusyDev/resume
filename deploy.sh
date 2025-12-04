14d05d462ea5cb8c0d70cbd37c262aa1192b1646
Pietro Businaro <pietro.businaro@mail.polimi.it>
2025-12-04 09:45:12 +0100

    Add generation of the résumé in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 # Deploy into GitHub pages.
 
+./generate.sh || \
+{
+  echo "An error occurred while generate your awesome résumé..."
+  exit 1
+}
+
 cd html/
 
 {
   git pull && \
   git commit -am "Update GitHub pages" && \
-  git push;
+  git push
 } \
 || \
 {
-  echo "An error occurred while uploading your page... :(";
-  exit 1;
+  echo "An error occurred while uploading your page... :("
+  exit 1
 }
 
 cd ..
